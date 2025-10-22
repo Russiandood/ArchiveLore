be4e5e4e3808b14825e55b4ee211de7ec4d01dda
Russiandood <stephenchernishoff@gmail.com>
2025-10-21 22:40:38 -0700

    Create page.tsx

---
+++ apps/web/app/(app)/page.tsx
@@ -0,0 +1,9 @@
+export default function AppHome() {
+  return (
+    <main className="p-8">
+      <h1>On-Stream Game</h1>
+      <p>Web app is alive. Sign in to start testing.</p>
+      {/* keep whatever you had on the old home, incl. AuthButton etc. */}
+    </main>
+  );
+}
