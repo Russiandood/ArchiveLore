ee8185aab5049c666db2040a00ddf869993b7673
Russiandood <stephenchernishoff@gmail.com>
2025-10-22 01:49:13 -0700

    Update layout.tsx

--- apps/web/app/(building)/layout.tsx
+++ apps/web/app/(building)/layout.tsx
@@ -1,8 +1,13 @@
-import '../builder-components';
+import BuilderRegistry from './BuilderRegistry';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default function BuildingLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen flex flex-col">{children}</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <BuilderRegistry />  {/* ensures custom components are registered on the client */}
+      {children}
+    </div>
+  );
 }
