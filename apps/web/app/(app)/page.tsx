188e1ce436520473a8947363adc4896d0b6b654b
Russiandood <stephenchernishoff@gmail.com>
2025-10-22 06:18:43 -0700

    Delete page.tsx

--- apps/web/app/(app)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AppHome() {
-  return (
-    <main className="p-8">
-      <h1>On-Stream Game</h1>
-      <p>Web app is alive. Sign in to start testing.</p>
-      {/* keep whatever you had on the old home, incl. AuthButton etc. */}
-    </main>
-  );
-}
